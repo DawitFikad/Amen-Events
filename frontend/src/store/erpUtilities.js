c214e9e96bc5c81e637dba0c63b809ad24de60ec
abel852 <abeltes555@gmail.com>
2026-09-27 00:06:31 +0300

    feat(store): export unified ERP utilities module for enterprise calculations

---
+++ frontend/src/store/erpUtilities.js
@@ -0,0 +1,34 @@
+/**
+ * Unified Amen Events ERP Utility Registry
+ * Exposes all enterprise calculations, security, and lifecycle modules.
+ */
+export * from './staffPayrollUtils.js';
+export * from './ticketPricingUtils.js';
+export * from './vendorContractUtils.js';
+export * from './badgeQrUtils.js';
+export * from './notificationDispatchUtils.js';
+export * from './auditLoggerUtils.js';
+export * from './analyticsMetricUtils.js';
+export * from './exhibitionBoothUtils.js';
+export * from './sponsorshipMatrixUtils.js';
+export * from './currencyExchangeUtils.js';
+export * from './taskSchedulerUtils.js';
+export * from './seatingArrangementUtils.js';
+export * from './assetDepreciationUtils.js';
+export * from './cateringDietaryUtils.js';
+export * from './speakerHonorariumUtils.js';
+export * from './marketingUtmUtils.js';
+export * from './feedbackNpsUtils.js';
+export * from './rfpBidEvaluationUtils.js';
+export * from './equipmentMaintenanceUtils.js';
+export * from './soundDecibelComplianceUtils.js';
+export * from './stagePowerLoadUtils.js';
+export * from './parkingCapacityUtils.js';
+export * from './volunteerShiftUtils.js';
+export * from './lostAndFoundUtils.js';
+export * from './wifiBandwidthUtils.js';
+export * from './eventInsuranceUtils.js';
+export * from './badgePrintQueueUtils.js';
+export * from './promoDiscountCodeUtils.js';
+export * from './eventSlaResolutionUtils.js';
+export * from './eventLifecycleTransitionUtils.js';
